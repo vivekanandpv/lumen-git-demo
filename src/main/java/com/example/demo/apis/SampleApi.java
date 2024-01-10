37ec49cea333b3b0fba65a5fe84dc06c5a747bd1
Gunashree P <gunashree.p@lumen.com>
2024-01-10 14:54:46 +0530

    gunashree commit

--- src/main/java/com/example/demo/apis/SampleApi.java
+++ src/main/java/com/example/demo/apis/SampleApi.java
@@ -10,6 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class SampleApi {
     @GetMapping
     public ResponseEntity<String> get() {
-        return ResponseEntity.ok("Hello, class!");
+        return ResponseEntity.ok("Hello, good afternoon!!");
     }
 }
